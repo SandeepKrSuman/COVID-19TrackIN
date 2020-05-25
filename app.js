3fc058582b9a9a2b95ff4a8a378a8f79c746ef72
Sandeep Suman <sandeepkrsuman2409@gmail.com>
2020-05-25 07:37:59 +0530

    added district data and some changes to the frontend

--- app.js
+++ app.js
@@ -1,109 +1,51 @@
 const express = require("express");
+const request = require("request");
 const app = express();
-const https = require("https");
-const mongoose = require('mongoose');
-const auth = require(__dirname + "/db.js");
+
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 
-mongoose.connect("mongodb+srv://" + auth() + "@cluster0-n1qle.mongodb.net/covidDB", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
-
-const caseSchema = new mongoose.Schema({
-  apidata: Object,
-  tincrease: Number,
-  rincrease: Number,
-  dincrease: Number,
-  date: String
-});
-
-const Case = mongoose.model('Case', caseSchema);
 
 
 app.get("/", function(req, res) {
 
-  const url = "https://covid19-mohfw.herokuapp.com/"
+  const url1 = "https://api.covid19india.org/data.json"
+  const url2 = "https://api.covid19india.org/v2/state_district_wise.json"
 
-  https.get(url, function(response) {
-    console.log(response.statusCode);
+  request(url1, function(error, response, body) {
+    const data = JSON.parse(body);
 
-    response.on("data", function(data) {
-      const covidData = JSON.parse(data)
+    // console.log(response.statusCode);
 
-      Case.find({}, function(err, found) {
+    request(url2, function(err, resp, bdy) {
+      const districtdata = JSON.parse(bdy);
 
-        if (found.length === 0) {
+      // console.log(resp.statusCode);
 
-          var currentTime = new Date();
-          var currentOffset = currentTime.getTimezoneOffset();
-          var ISTOffset = 330;   // IST offset UTC +5:30
-          var ISTTime = new Date(currentTime.getTime() + (ISTOffset + currentOffset)*60000);
+      if (response.statusCode != 200 && resp.statusCode != 200) {
+        console.log("API 1 : " + response.statusCode);
+        console.log("API 2 : " + resp.statusCode);
+        console.log("API did not connect correctly, redirecting!!");
+        res.redirect("/");
+      }
 
-          const defaultCase = new Case({
-            apidata: covidData,
-            tincrease: 0,
-            rincrease: 0,
-            dincrease: 0,
-            date: ISTTime
-          });
-
-          defaultCase.save();
-          console.log("Default Data added successfully, Redirecting");
-          res.redirect("/");
-
-        } else {
-
-          if (found[0].apidata.totals.total !== covidData.totals.total) {
-            const tinc = covidData.totals.total - found[0].apidata.totals.total;
-            const rinc = covidData.totals.recoveries - found[0].apidata.totals.recoveries;
-            const dinc = covidData.totals.deaths - found[0].apidata.totals.deaths;
-
-
-            Case.deleteMany({}, function() {
-              console.log("Last data Deletedfrom DB");
-            });
-
-            var currentTime = new Date();
-            var currentOffset = currentTime.getTimezoneOffset();
-            var ISTOffset = 330;   // IST offset UTC +5:30
-            var ISTTime = new Date(currentTime.getTime() + (ISTOffset + currentOffset)*60000);
-
-            const addingnew = new Case({
-              apidata: covidData,
-              tincrease: tinc,
-              rincrease: rinc,
-              dincrease: dinc,
-              date: ISTTime
-            });
-            addingnew.save();
-            console.log("New Data added to DB");
-          }
-
-
-          res.render("index", {
-            covidData: covidData,
-            tincr: found[0].tincrease,
-            rincr: found[0].rincrease,
-            dincr: found[0].dincrease,
-            updateDate: found[0].date
-          });
-
-        }
-
+      res.render("index", {
+        covidData: data,
+        disCovidData: districtdata
       });
+    });
 
-    })
-  })
+  });
 
 });
 
+
+
 let port = process.env.PORT;
-if (port == null || port == ""){
+if (port == null || port == "") {
   port = 3000;
 }
-app.listen( port, function() {
+app.listen(port, function() {
   console.log("Server has started Successfully");
 })
